42c6a9b36ee5752d332ad3ae239bf5ab733f40fa
Artur <iphone3034890@gmail.com>
2024-10-06 19:44:40 +0500

    main

--- src/app/[locale]/components/shared/Burger_profile.js
+++ src/app/[locale]/components/shared/Burger_profile.js
@@ -2,8 +2,8 @@
 import { useState } from "react";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 import { useThemeStore } from "../../../../store";
+import { BurgerIcon } from "../shared/BurgerIcon";
 
 export const Burger_profile = () => {
   const [show, setShow] = useState(true);
@@ -20,19 +20,13 @@ export const Burger_profile = () => {
   return (
     <>
       <div className={listClass}>
-        <div className='py-24 px-10'>
-					<h1 className='text-[5rem] text-teal-500'>NEW CONTENT WILL BE HERE</h1>
-				</div>
+        <div className="py-24 px-10">
+          <h1 className="text-[5rem] text-teal-500">NEW CONTENT WILL BE HERE</h1>
+        </div>
       </div>
 
       <div className={classChange} onClick={handleClick}>
-        <Image
-          color={theme === "dark" ? "white" : "black"}
-          src={"/header_icons/set.svg"}
-          width={30}
-          height={30}
-          alt={"Burger menu"}
-        />
+        <BurgerIcon color={theme === "dark" ? "white" : "black"} />
       </div>
     </>
   );
