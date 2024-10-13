b03d899c2b37c05e71d68bebaf022e4d331af949
Artur <iphone3034890@gmail.com>
2024-10-13 19:42:12 +0500

    main

--- src/components/shared/ChangeNick.tsx
+++ src/components/shared/ChangeNick.tsx
@@ -39,73 +39,68 @@ export const ChangeNick: NextPage<Props> = ({ propsItem }) => {
 				</Button>
 			</DrawerTrigger>
 
-			<DrawerContent className='px-[15px] min-h-[100dvh]'>
-				
+			<DrawerContent className='px-[15px]'>
+				<div className='w-full border-transparent border-b-1 border-solid border-b-gray-400 pb-[20px] mb-[40px]'>
+					<h1 className='text-[20px] md:text-[32px] flex items-center gap-[15px]'>
+						<DrawerClose asChild>
+							<span className='flex items-center gap-[5px] text-[18px] lg:text-[30px]'>
+								<span className='text-[18px] lg:text-[30px]'>{'<'}</span>
+							</span>
+						</DrawerClose>
+						Nickname
+					</h1>
+				</div>
 
-					<div className='w-full border-transparent border-b-1 border-solid border-b-gray-400 pb-[20px] mb-[40px]'>
-						<h1 className='text-[20px] md:text-[32px] flex items-center gap-[15px]'>
-							<DrawerClose asChild>
-								<span className='flex items-center gap-[5px] text-[18px] lg:text-[30px]'>
-									<span className='text-[18px] lg:text-[30px]'>{'<'}</span>
-								</span>
-							</DrawerClose>
-							Nickname
-						</h1>
-					</div>
-
-					<div className='flex flex-col justify-flex-start items-center w-full h-full'>
-						<div className='max-w-[760px] flex flex-col gap-[10px]'>
-							<div className='text-black dark:text-white bg-[#F5F5F5] dark:bg-[#181818] py-[24px] px-[22px] rounded-[6px] flex flex-col items-center md:flex-row md:items-start gap-[16px]'>
-								<Image
-									src={'/header_icons/profile_burger/info_icon.svg'}
-									width={40}
-									height={40}
-									alt='info'
-									quality={100}
-									className='w-full max-w-[20px] sm:max-w-[23px] md:max-w-[25px]'
-								/>
-								<span className='text-black dark:text-white text-[14px] md:text-[18px] lg:text-[20px] 2xl:text-[25px] text-left'>
-									Make sure your nickname does not contain disrespectful
-									language, official names(i.e. product names), or names of
-									other trading platforms.
-								</span>
-							</div>
-							<div className='flex flex-col gap-[14px] lg:gap-[40px]'>
-								<span className='text-black dark:text-white text-left text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[20px]'>
-									Your nickname is used for trading and in-app messages
-								</span>
-								<input
-									type='text'
-									className='bg-transparent text-black dark:text-white border border-solid border-[#000] dark:border-white rounded-[4px] px-[20px] py-[7px] text-[16px] md:text-[18px] lg:text-[20px] 2xl:text-[25px]'
-									onChange={trackSymbols}
-									placeholder='zya***@rambler.ru'
-								/>
-								<span className='w-full flex justify-between text-black dark:text-white text-left text-[14px] md:text-[18px] lg:text-[20px] 2xl:text-[25px]'>
-									Your nickname can be edited 3 more time(s) this year{' '}
-									<span className='text-black dark:text-white'>
-										{symbols}/20
-									</span>
-								</span>
-							</div>
+				<div className='flex flex-col justify-flex-start items-center w-full h-full'>
+					<div className='max-w-[760px] flex flex-col gap-[10px]'>
+						<div className='text-black dark:text-white bg-[#F5F5F5] dark:bg-[#181818] py-[24px] px-[22px] rounded-[6px] flex flex-col items-center md:flex-row md:items-start gap-[16px]'>
+							<Image
+								src={'/header_icons/profile_burger/info_icon.svg'}
+								width={40}
+								height={40}
+								alt='info'
+								quality={100}
+								className='w-full max-w-[20px] sm:max-w-[23px] md:max-w-[25px]'
+							/>
+							<span className='text-black dark:text-white text-[14px] md:text-[18px] lg:text-[20px] 2xl:text-[25px] text-left'>
+								Make sure your nickname does not contain disrespectful language,
+								official names(i.e. product names), or names of other trading
+								platforms.
+							</span>
+						</div>
+						<div className='flex flex-col gap-[14px] lg:gap-[40px]'>
+							<span className='text-black dark:text-white text-left text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[20px]'>
+								Your nickname is used for trading and in-app messages
+							</span>
+							<input
+								type='text'
+								className='bg-transparent text-black dark:text-white border border-solid border-[#000] dark:border-white rounded-[4px] px-[20px] py-[7px] text-[16px] md:text-[18px] lg:text-[20px] 2xl:text-[25px]'
+								onChange={trackSymbols}
+								placeholder='zya***@rambler.ru'
+							/>
+							<span className='w-full flex justify-between text-black dark:text-white text-left text-[14px] md:text-[18px] lg:text-[20px] 2xl:text-[25px]'>
+								Your nickname can be edited 3 more time(s) this year{' '}
+								<span className='text-black dark:text-white'>{symbols}/20</span>
+							</span>
 						</div>
 					</div>
+				</div>
 
-					<div className='flex flex-row justify-center gap-[40px] mt-[15px]'>
-						<DrawerClose asChild>
-							<Button
-								variant='outline'
-								className='border border-solid dark:border-gray-300 border-black rounded-[50px] px-[35px] min-w-[117px] hover:bg-transparent hover:text-[#205BC9] text-[14px] md:text-[18px] lg:text-[20px] lg:px-[30px] lg:py-[15px]'
-							>
-								Cancel
-							</Button>
-						</DrawerClose>
-						<DrawerClose asChild>
-							<Button className='bg-[#205BC9] text-white rounded-[50px] px-[35px] min-w-[117px] hover:bg-[#205BC9] hover:text-[#fff] text-[14px] md:text-[18px] lg:text-[20px] lg:px-[30px] lg:py-[15px]'>
-								Confirm
-							</Button>
-						</DrawerClose>
-					</div>
-				
+				<div className='flex flex-row justify-center gap-[40px] mt-[15px]'>
+					<DrawerClose asChild>
+						<Button
+							variant='outline'
+							className='border border-solid dark:border-gray-300 border-black rounded-[50px] px-[35px] min-w-[117px] hover:bg-transparent hover:text-[#205BC9] text-[14px] md:text-[18px] lg:text-[20px] lg:px-[30px] lg:py-[15px]'
+						>
+							Cancel
+						</Button>
+					</DrawerClose>
+					<DrawerClose asChild>
+						<Button className='bg-[#205BC9] text-white rounded-[50px] px-[35px] min-w-[117px] hover:bg-[#205BC9] hover:text-[#fff] text-[14px] md:text-[18px] lg:text-[20px] lg:px-[30px] lg:py-[15px]'>
+							Confirm
+						</Button>
+					</DrawerClose>
+				</div>
 			</DrawerContent>
 		</Drawer>
 	)
