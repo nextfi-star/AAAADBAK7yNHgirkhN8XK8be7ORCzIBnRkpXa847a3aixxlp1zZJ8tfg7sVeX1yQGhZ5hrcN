792dc7ea9f8e33dea8acd726d04e420161ec8ffc
Artur <iphone3034890@gmail.com>
2024-10-21 00:10:27 +0500

    main

--- src/components/shared/Platform_mode.tsx
+++ src/components/shared/Platform_mode.tsx
@@ -4,15 +4,14 @@ import React from 'react'
 export const Platform_mode = () => {
 	return (
 		<div className='flex items-center bg-white dark:bg-black border border-solid border-black dark:border-white rounded-[50px] gap-[10px]'>
-			<button className='lock-btn flex items-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+			<button className='lock-btn flex items-center justify-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] min-w-[99px] gap-[3px] text-[14px] bg-[#205BC9]'>
 				Platform
-				<Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
 			</button>
-			<button className='lock-btn flex items-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+			<button className='lock-btn flex items-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px] min-w-[99px]'>
 				Exchange
 				<Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
 			</button>
-			<button className='lock-btn flex items-center min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+			<button className='lock-btn flex items-center min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px] min-w-[99px] justify-center'>
 				Web 3 <Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
 			</button>
 		</div>
