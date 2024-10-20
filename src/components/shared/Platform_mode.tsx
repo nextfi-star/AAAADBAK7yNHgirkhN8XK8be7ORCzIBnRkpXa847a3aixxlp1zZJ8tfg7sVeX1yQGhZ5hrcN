2cc4ad5a1ea8015a6d3848ac2307f1b064aeeb12
Artur <iphone3034890@gmail.com>
2024-10-21 00:05:34 +0500

    main

--- src/components/shared/Platform_mode.tsx
+++ src/components/shared/Platform_mode.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Platform_mode = () => {
 	return (
-		<div className=''>
-		
+		<div className='flex items-center bg-white dark:bg-black border border-solid border-black dark:border-white rounded-[50px] gap-[10px]'>
+			<button className='lock-btn flex items-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+				Platform
+				<Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
+			</button>
+			<button className='lock-btn flex items-center border-r border-0 border-solid border-r-black dark:border-r-white rounded-[50px] min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+				Exchange
+				<Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
+			</button>
+			<button className='lock-btn flex items-center min-h-[30px] max-h-[30px] px-[8px] gap-[3px] text-[14px]'>
+				Web 3 <Image src={'/lock.svg'} width={20} height={20} alt='lock icon' className='lock max-w-[15px]' />
+			</button>
 		</div>
 	)
 }
