e79c0fb67624914689f4cc735cbd58da924b979c
Artur <iphone3034890@gmail.com>
2024-11-05 19:07:55 +0500

    main

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const createNextIntlPlugin = require('next-intl/plugin');
- 
-const withNextIntl = createNextIntlPlugin();
- 
+const createNextIntlPlugin = require('next-intl/plugin')
+
+const withNextIntl = createNextIntlPlugin()
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
-module.exports = withNextIntl(nextConfig);
+const nextConfig = { ignoreDuringBuilds: true }
+
+module.exports = withNextIntl(nextConfig)
